e2bd36e3c859a920e49315a7e12c50f95dedb072
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-11 18:34:30 +0000

    alhamdo li Allah started making the skeleton of the website

--- bismi_allah.com/account/login.php
+++ bismi_allah.com/account/login.php
@@ -1,7 +1,18 @@
 <?php
 //in the name of Allah
-
-echo "in the name of Allah";
-echo "<br>page under construction"
-
+session_set_cookie_params(500);
+session_start();
 ?>
+<!DOCTYPE html>
+<!--in the name of Allah-->
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../res/bismi_allah.css">
+    <title>in the name of Allah</title>
+</head>
+<body>
+    <p>in the name of Allah</p>
+</body>
+</html>
+
--- bismi_allah.com/bismi_allah.php
+++ bismi_allah.com/bismi_allah.php
@@ -1,12 +1,20 @@
+<?php
+//in the name of Allah
+include_once('../../scripts/bismi_allah.php');
+session_set_cookie_params(500);
+session_start();
+?>
 <!DOCTYPE html>
 <!--in the name of Allah-->
 <html>
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="res/bismi_allah.css">
     <title>in the name of Allah</title>
 </head>
 <body>
     <p>in the name of Allah</p>
+    <?php if(isset($_SESSION['user_name'])){echo '<p>' . $_SESSION['user_name'] . '</p>';} ?>
     <h2>account</h2>
     <ul>
         <li><a href="account">account</a></li>
