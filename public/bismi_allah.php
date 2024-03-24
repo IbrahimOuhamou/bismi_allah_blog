5dc792c983a86d6cf2d403ad21379fb89d716d5c
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-03-24 23:24:07 +0000

    alhamdo li Allah

--- public/bismi_allah.php
+++ public/bismi_allah.php
@@ -13,20 +13,46 @@ $bismi_allah_request = explode("/", htmlspecialchars($_SERVER['REQUEST_URI']));
     <link rel="stylesheet" href="/res/bismi_allah.css" type="text/css">
 </html>
 <body>
+    
+    <header>
+        <a href="/">home</a>
+        <a href="/account">account</a>
+    </header>
+    <p>la ilaha illa Allah Mohammed rassoul Allah</p>
+
 <?php
-    echo "<p>la ilaha illa Allah Mohammed rassoul Allah</p>";
-    print_r($bismi_allah_request);
 if(0 === strcmp($bismi_allah_request[1], ''))
 {
-    echo '<p>requested /</p>';
+    echo '<h1>requested \'/\'</h1>';
 }
 elseif(0 === strcmp($bismi_allah_request[1], 'blogs'))
 {
-    echo '<p>requested \'blogs\'</p>';
+    echo '<h1>requested \'blogs\'</h1>';
+    if(isset($bismi_allah_request[2]))
+    {
+        $blog_id = $bismi_allah_request[2];
+        echo '<p><b>blog id is ' . $blog_id . ' </b></p>';
+        if(is_numeric($blog_id))
+        {
+            echo '<p>alhamdo li Allah blg id is <b style="color:green;">VALID</b></p>';
+        }
+        else
+        {
+            echo '<p style="color: red;"><b>BLOG ID IS NOT VALID</b></p>';
+        }
+    }
+}
+elseif(0 === strcmp($bismi_allah_request[1], 'users'))
+{
+    echo '<h1>requested \'users\'</h1>';
 }
 elseif(0 === strcmp($bismi_allah_request[1], 'account'))
 {
-    echo '<p>requested \'account\'</p>';
+    echo '<h1>requested \'account\'</h1>';
+}
+else
+{
+    echo '<h1>PAGE NOT FOUND</h1>';
 }
 ?>
 </body>
