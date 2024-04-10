89685e8bd86d5e1068d4f338ed130c4d1d3252e2
IbrahimOuhamou <ouhamouy10@gmail.com>
2024-04-10 20:14:32 +0000

    alhamdo li Allah tried to migrate to mysql

--- django/bismi_allah_blog/bismi_allah_blog/settings.py
+++ django/bismi_allah_blog/bismi_allah_blog/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'bismi_allah_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bismi_allah_blog',
+        'USER': 'bismi_allah',
+        'PASSWORD': 'bismi_allah',
+        'HOST': 'localhost',
     }
 }
 
